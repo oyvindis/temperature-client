7c7433f4ede2346d1a1a352fcbd1721713b20dab
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-05 20:56:42 +0200

    feat: update main-page design from material-ui

--- src/pages/main-page/main-page-pure.tsx
+++ src/pages/main-page/main-page-pure.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import {Grid, Card, Typography, CardContent, Container, CardHeader, Box} from "@material-ui/core";
 
-export const MainPagePure = ({ temperature }: any): JSX.Element | null => (
-  <div>
-    <h1>Temperatures from API:</h1>
-    <div>
-      {temperature.location} - {temperature.temperature} - {temperature.time}
-    </div>
-  </div>
+interface Props {
+  temperature?: {
+    location: string;
+    temperature: string;
+    time: string;
+  }
+}
+export const MainPagePure: React.FC<Props> = ({ temperature }) => (
+  <Container maxWidth="sm" component="main">
+    <Grid container style={{padding: 24}}>
+      <div>
+        <Typography gutterBottom component="h1">Temperatures from API:</Typography>
+        {temperature && (
+          <Card >
+            <CardHeader
+              title={temperature.location}
+              titleTypographyProps={{ align: 'center' }}
+              subheaderTypographyProps={{ align: 'center' }}
+            />
+            <CardContent>
+              <Box display="flex" justifyContent="center" mb={2}>
+                <Typography variant="h5" color="textPrimary">
+                  Yr:&nbsp;
+                </Typography>
+                <Typography variant="h5" color={Number(temperature.temperature) > 0 ? 'secondary' : 'primary'}>
+                  {temperature.temperature}°
+                </Typography>
+              </Box>
+              <Typography variant="h6" color="textSecondary">
+                {temperature.time}
+              </Typography>
+            </CardContent>
+          </Card>
+        )}
+      </div>
+    </Grid>
+  </Container>
 );
