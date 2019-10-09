8150221307c632b8bcec59f0a99736bc6faa2522
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-09 19:50:39 +0200

    chore: add react-hot-loader to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
 import './index.css';
 import App from './app/App';
-import * as serviceWorker from './serviceWorker';
+
 import { loadConfig } from './config';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function run(): void {
+  render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+run();
+
+if ((module as any).hot) {
+  (module as any).hot.accept();
+}
 
 loadConfig().catch(console.error);
