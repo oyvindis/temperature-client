ee9af89aa87599eb562fcaa95ddb607ba8288dec
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-10-11 19:28:01 +0200

    chore: update eslintrc and packages

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -2,7 +2,7 @@ import { compose } from 'recompose';
 import { mainPageResolver } from './main-page-resolver';
 import { MainPagePure } from './main-page-pure';
 
-//const mapRouteParams = withProps(({ match: { params } }) => _.pick(params, 'catalogId'));
+// const mapRouteParams = withProps(({ match: { params } }) => _.pick(params, 'catalogId'));
 
 const enhance = compose(mainPageResolver);
 export const MainPage = enhance(MainPagePure);
